eaf718441e914daf033e49f5018decab7d5a91e1
Denis-de-Bernardy <Denis-de-Bernardy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-01-06 11:52:00 +0000

    sem-opt-in-front 3.1.1
    git-svn-id: https://plugins.svn.wordpress.org/sem-opt-in-front/trunk@84536 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sem-opt-in-front.php
+++ sem-opt-in-front.php
@@ -4,8 +4,8 @@ Plugin Name: Opt-in Front Page
 Plugin URI: http://www.semiologic.com/software/publishing/opt-in-front/
 Description: Restricts the access to your front page on an opt-in basis: Only posts within the category with a slug of 'blog' will be displayed on your front page.
 Author: Denis de Bernardy
-Version: 3.1
-Author URI: http://www.semiologic.com
+Version: 3.1.1
+Author URI: http://www.getsemiologic.com
 */
 
 /*
@@ -124,7 +124,7 @@ class sem_opt_in_front
 				}
 				else
 				{
-					$new_link = get_option('home');
+					$new_link = user_trailingslashit(get_option('home'));
 				}
 			}
 			
